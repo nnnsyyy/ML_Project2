6825ceef30b68d0213336f2ed82d5de9fea85d95
Jiacheng Xu <xjcmaxwell@163.com>
2016-12-12 20:26:55 +0100

    Tried to use the least-square to complete the first try.

--- project_text_classification/cooc.py
+++ project_text_classification/cooc.py
@@ -5,13 +5,13 @@ import pickle
 
 
 def main():
-    with open('vocab.pkl', 'rb') as f:
+    with open('./data/vocab.pkl', 'rb') as f:
         vocab = pickle.load(f)
     vocab_size = len(vocab)
 
     data, row, col = [], [], []
     counter = 1
-    for fn in ['pos_train.txt', 'neg_train.txt']:
+    for fn in ['./twitter-datasets/pos_train.txt', './twitter-datasets/neg_train.txt']:
         with open(fn) as f:
             for line in f:
                 tokens = [vocab.get(t, -1) for t in line.strip().split()]
@@ -28,7 +28,7 @@ def main():
     cooc = coo_matrix((data, (row, col)))
     print("summing duplicates (this can take a while)")
     cooc.sum_duplicates()
-    with open('cooc.pkl', 'wb') as f:
+    with open('./data/cooc.pkl', 'wb') as f:
         pickle.dump(cooc, f, pickle.HIGHEST_PROTOCOL)
 
 
