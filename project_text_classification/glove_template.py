ade5e49fae7b6446280ba81d320da27aeb491900
Jiacheng Xu <xjcmaxwell@163.com>
2016-12-22 02:42:57 +0100

    Add pca in notebook.

--- project_text_classification/glove_template.py
+++ project_text_classification/glove_template.py
@@ -7,7 +7,7 @@ import random
 
 def main():
     print("loading cooccurrence matrix")
-    with open('cooc.pkl', 'rb') as f:
+    with open('./newdata_/cooc.pkl', 'rb') as f:
         cooc = pickle.load(f)
     print("{} nonzero entries".format(cooc.nnz))
 
@@ -15,35 +15,52 @@ def main():
     print("using nmax =", nmax, ", cooc.max() =", cooc.max())
 
     print("initializing embeddings")
-    embedding_dim = 20
+    embedding_dim = 50
     xs = np.random.normal(size=(cooc.shape[0], embedding_dim))
     ys = np.random.normal(size=(cooc.shape[1], embedding_dim))
 
     eta = 0.001
     alpha = 3 / 4
 
-    epochs = 10
+    epochs = 100
+    gamma = 20
+    
+    min_mse=1000
+    mse = 0
+    best_xs = np.zeros((cooc.shape[0], embedding_dim))
 
     for epoch in range(epochs):
+        pre_mse = mse
         mse = 0
         grad_xs = np.zeros((cooc.shape[0], embedding_dim))
         grad_ys = np.zeros((cooc.shape[1], embedding_dim))
-        gamma = 10/int(1+epoch/20)
-        print("epoch {}".format(epoch)+"gamma {}".format(gamma))
+				
+        print("epoch {}".format(epoch)+" gamma {}".format(gamma))
         for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
             logn = np.log(n)
             fn = min(1.0, (n / nmax) ** alpha)
             x, y = xs[ix, :], ys[jy, :]
-            scale = 2 * eta * fn * (logn - np.dot(x, y))
-            mse_n = (scale/2)**2/(fn*eta)
+            #scale = 2 * eta * fn * (logn - np.dot(x, y))
+            mse_n = 1/2 * (logn - np.dot(x, y))**2 * (fn) * eta
             mse += mse_n
-            grad_xs[ix,:] += -scale * y
-            grad_ys[jy,:] += -scale * x
+            grad_xs[ix,:] -= (logn - np.dot(x, y)) * fn * y * eta
+            grad_ys[jy,:] -= (logn - np.dot(x, y)) * fn * x * eta
         print("mse {}".format(mse))
-        xs += -gamma * grad_xs
-        ys += -gamma * grad_ys
-
-    np.save('embeddings', xs)
+        xs -= gamma * grad_xs
+        ys -= gamma * grad_ys
+		
+        if(min_mse > mse):
+            min_mse = mse
+            best_xs = xs
+            
+        if(epoch>=10):
+            gamma = gamma*(1-1/(1+epoch*5))
+            if(mse-min_mse > 1 or abs(pre_mse-mse) < 0.1):
+                break
+       
+    print("best gamma {}".format(gamma))
+    print("min_mse {}".format(min_mse))
+    np.save('./newdata_/embeddings_dim50g10', best_xs)
 
 
 if __name__ == '__main__':
--- project_text_classification/helpers.py
+++ project_text_classification/helpers.py
@@ -22,21 +22,21 @@ def sentence_metrix(path,vocab,embed_word_metrix):
         with open(fn) as f:
             for line in f:
                 word_num=0
-                sentence_vector = np.zeros(20)
+                sentence_vector = np.zeros(embed_word_metrix.shape[1])
                 for t in line.strip().split(): 
                     if(vocab.get(t)!=None):
-                        sentence_vector +=embed_word_metrix[vocab.get(t)-1,:]
+                        sentence_vector +=embed_word_metrix[vocab.get(t),:]
                         word_num+=1
                 if(word_num!=0):
                     sentence_vector = sentence_vector/word_num
                 else:
-                    sentence_vector = np.zeros(20)  
+                    sentence_vector = np.zeros(embed_word_metrix.shape[1])  
                 sentence_metrix.append(sentence_vector)
     return sentence_metrix
 
 def build_train(vocab,embed_word_metrix):
-    s_metrix_pos = sentence_metrix("./twitter-datasets/train_pos_full_.txt", vocab,embed_word_metrix)
-    s_metrix_neg = sentence_metrix("./twitter-datasets/train_neg_full_.txt",vocab,embed_word_metrix)
+    s_metrix_pos = sentence_metrix("./newdata_/train_pos_full_.txt", vocab,embed_word_metrix)
+    s_metrix_neg = sentence_metrix("./newdata_/train_neg_full_.txt",vocab,embed_word_metrix)
     s_metrix_pos = np.asarray(s_metrix_pos)
     s_metrix_neg = np.asarray(s_metrix_neg)
     s_metrix_pos = np.insert(s_metrix_pos,0,1,axis = 1)
@@ -45,13 +45,21 @@ def build_train(vocab,embed_word_metrix):
     return s_me
 
 def build_test(vocab,embed_word_metrix):
-    s_me_test = sentence_metrix("./twitter-datasets/test_data_.txt",vocab,embed_word_metrix)
+    s_me_test = sentence_metrix("./newdata_/test_data_.txt",vocab,embed_word_metrix)
     s_me_test = np.asarray(s_me_test)
     return s_me_test
 
 
 def build_sample(train_all, size):
-    #train_sample = train_all[np.random.randint(train_all.shape[0],size = size),:]
+    train_sample = train_all[np.random.randint(train_all.shape[0],size = size),:]
     y_train_sample = train_all[:,0]
     x_train_sample = np.delete(train_all,0,axis=1)
     return x_train_sample,y_train_sample
+
+def normallize(metrix):
+    metrix_nor = np.zeros((metrix.shape[0],metrix.shape[1]))
+    for column in range(metrix.shape[1]):
+        max = np.amax(metrix[:,column])
+        metrix_nor[:,column] = metrix[:,column]/max
+    return metrix_nor
+
